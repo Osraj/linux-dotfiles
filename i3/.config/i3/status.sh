083c63ae382e070384007bd9d24081a6a8ee6fd5
Osraj <46506381+Osraj@users.noreply.github.com>
2026-03-08 23:35:26 +0300

    fix: resolve bash syntax errors in battery status scripts
    - add missing $ prefix and [[ ]] syntax for variable comparisons
    - fix notified_15 and notified_5 variable references

--- i3/.config/i3/status.sh
+++ i3/.config/i3/status.sh
@@ -7,14 +7,14 @@ while true; do
         BATTERY=$(cat /sys/class/power_supply/BAT0/capacity)
         STATUS=$(cat /sys/class/power_supply/BAT0/status)
         BAT_DISPLAY="BAT: $BATTERY% ($STATUS)"
-        if [BATTERY -gt 15]; then
+        if [[ $BATTERY -gt 15 ]]; then
             notified_15=0
             notified_5=0
-        elif [[ BATTERY -lt 15 ]] && [[ notified_15 -eq 0 ]]; then
+        elif [[ $BATTERY -lt 15 ]] && [[ $notified_15 -eq 0 ]]; then
             notify-send "Battery" "Battery reached 15%"
             notified_5=0
             notified_15=1
-        elif [[ BATTERY -lt 5 ]] && [[ notified_5 -eq 0 ]]; then
+        elif [[ $BATTERY -lt 5 ]] && [[ $notified_5 -eq 0 ]]; then
             notify-send "Battery" "Battery reached 5%"
             notified_5=1
             notified_15=1
