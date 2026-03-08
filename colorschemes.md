f6410dea1489a477ee6a5174a411190ba1c61a9b
Osraj <46506381+Osraj@users.noreply.github.com>
2026-03-08 22:02:30 +0300

    chore: remove old flat-file layout and update gitignore
    - removes legacy flat files replaced by new directory structure
    - updates .gitignore to generic editor/OS patterns

--- colorschemes.md
+++
@@ -1,2 +0,0 @@
-# list of my favourite terminal colorschemes :]
-- [catppuccin-mocha](https://github.com/catppuccin/xfce4-terminal)
--- keybinds.sh
+++
@@ -1,4 +0,0 @@
-bindkey '^[[A' history-substring-search-up\n
-bindkey '^[[B' history-substring-search-down\n
-bindkey -v\n
-export KEYTIMEOUT=1
